d2df3997770e0d9bcc3842481727bd676071dcfe
Joel Spadin <joelspadin@gmail.com>
2023-03-18 01:29:57 -0500

    refactor: Deduplicate key code decoding
    Unified devicetree key code parameter decoding between key press and
    caps word behaviors.
    
    Reordered zmk_keycode_state_changed fields for better packing.

--- app/include/zmk/keys.h
+++ app/include/zmk/keys.h
@@ -7,7 +7,9 @@
 #pragma once
 
 #include <zephyr/kernel.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
 #include <dt-bindings/zmk/keys.h>
+#include <dt-bindings/zmk/modifiers.h>
 
 typedef uint32_t zmk_key_t;
 typedef uint8_t zmk_mod_t;
@@ -19,6 +21,21 @@ struct zmk_key_event {
     zmk_key_t key;
     bool pressed;
 };
+
+struct zmk_key_decoded {
+    zmk_mod_flags_t modifiers;
+    uint8_t page;
+    uint16_t id;
+};
+
+/**
+ * Convert a uint32_t devicetree key code parameter to a struct zmk_key_decoded.
+ */
+#define ZMK_KEY_DECODE(encoded)                                                                    \
+    (struct zmk_key_decoded) {                                                                     \
+        .modifiers = SELECT_MODS(encoded), .page = ZMK_HID_USAGE_PAGE(encoded),                    \
+        .id = ZMK_HID_USAGE_ID(encoded),                                                           \
+    }
 
 static inline bool is_mod(uint8_t usage_page, uint32_t keycode) {
     return (keycode >= HID_USAGE_KEY_KEYBOARD_LEFTCONTROL &&
